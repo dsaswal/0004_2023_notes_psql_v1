11e7892a5128747a3a6d8e51fe4a8e829acb4d90
EC2 Default User <ec2-user@ip-172-31-9-70.eu-north-1.compute.internal>
2023-06-11 21:13:42 +0000

    first commit from local repo

---
+++ src/main/java/Trial.java
@@ -0,0 +1,23 @@
+package dsa.personal.trial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class Trial {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hoffedumichsehen!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Trial.class, args);
+    }
+
+}
+
+
